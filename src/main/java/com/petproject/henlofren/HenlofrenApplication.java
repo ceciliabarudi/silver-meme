934bca9b01a16369baa79830261b027b9505cb86
Cecilia Barudi <cecilia.barudiortiz@thoughtworks.com>
2020-07-02 09:45:50 +0200

    Initialize Spring project

---
+++ src/main/java/com/petproject/henlofren/HenlofrenApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.henlofren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenlofrenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenlofrenApplication.class, args);
+	}
+
+}
