b3622bb2bbb5f38f2723fb269b5c8413dab0b46e
Cecilia Barudi <cecilia.barudiortiz@thoughtworks.com>
2020-08-21 16:20:27 +0200

    Replace AutoConfigureMvc for WebMvcTest annotation in henlo controller test

--- src/test/java/com/petproject/henlofren/HenloControllerTest.java
+++ src/test/java/com/petproject/henlofren/HenloControllerTest.java
@@ -1,20 +1,20 @@
 package com.petproject.henlofren;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HenloController.class)
 public class HenloControllerTest {
 
     @Autowired
@@ -33,4 +33,6 @@ public class HenloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("dog")));
     }
+
+    // edge cases here
 }
