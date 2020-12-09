11a5d8d4ff307ba24d6f9ec51f9a2ed171051068
ceciliabarudi <blasterkat101@gmail.com>
2020-12-09 15:58:29 +0100

    Fix e2e test to avoid coding ui

--- src/test/cypress/integration/animals.spec.js
+++ src/test/cypress/integration/animals.spec.js
@@ -4,11 +4,11 @@ context('Pet project', () => {
     describe('Viewing a resource', function () {
         it('should display list of existing animals', function () {
             cy.visit('/animals')
-            cy.get('[data-test="animalList"] li').first().should('have.value', 'dog')
+            cy.contains('Dog, Cat, Platypus, Raccoon, Duck')
         });
         it('should display existing animal when requested by id', function () {
-            cy.visit('/animals/s0m3D0g1d')
-            cy.contains('dog')
+            cy.visit('/animals/3')
+            cy.contains('Platypus')
         });
     });
 })
